6cb21b1506fb37eb076f0dc947af6b81e7044934
asciishell <asciishell@yandex.ru>
2018-10-06 22:24:02 +0300

    Base structure for gender and random generator

---
+++ src/main/java/Rand.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Rand {
+    public static int RandomInt(int min, int max) {
+        Random random = new Random();
+        return min + random.nextInt(max - min + 1);
+    }
+    public static String RandomItem(String[] array){
+        Random random = new Random();
+        return array[random.nextInt(array.length)];
+    }
+}
